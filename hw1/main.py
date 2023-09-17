69fc17fbadf67fbaed80742b426d7a893b14bde7
aidancaputi <aidancaputi@gmail.com>
2023-09-17 15:17:05 -0400

    fully connected layer works for basic example

--- hw1/main.py
+++ hw1/main.py
@@ -1,12 +1,34 @@
 from datasets import *
+from nn import *
 
 def main():
 
     #create a dict where keys are 'x', 'y', and 'out'
     xor_data = xor_dataset()
+    print("\nXOR dataset: ")
     print(xor_data)
 
+    #1 -> number of layers
+    #2 -> input dimensions
+    #3 -> output dimensions
+    #4 -> hidden units in each layer
+    #5 -> learning rate
+    nn = create_nn(1, 2, 3, 4, 5)
+    print("\nNeural network: ")
+    print(nn)
     
+    #test fully connected layer
+    input_vec = [1,2,3]
+    weights = [[1, 2],
+               [3, 4],
+               [5, 6]]
+    bias = [3, 3]
+    print("\nFully connected layer test: ")
+    print("input: ", input_vec)
+    print("weights: ", *weights, sep='\n')
+    print("bias: ", bias)
+    print("gives", fc_linear_layer(input_vec, weights, bias))
+
 
 if __name__ == "__main__":
     main()
---
+++ hw1/nn.py
@@ -0,0 +1,47 @@
+#created a dictionary with network information
+def create_nn(n_layers, in_dim, out_dim, hl_size, learning_rate):
+    nn = {}
+    nn.update({'n_layers':n_layers})
+    nn.update({'in_dim':in_dim})
+    nn.update({'out_dim':out_dim})
+    nn.update({'hl_size':hl_size})
+    nn.update({'learning_rate':learning_rate})
+    return nn
+
+#multiplies input vector and weights
+def matmul(input_vec, weights_matrix):
+
+    #make sure the two vectors are going to work together in terms of dimensions
+    if(len(input_vec) != len(weights_matrix)):
+        return "Error multiplying " + input_vec + " and " + weights_matrix + ", their dimensions don't match!"
+
+    output = []
+    
+    #for each column in weights matrix
+    for i in range(len(weights_matrix[0])):
+        col_sum = 0
+
+        #go through input vec
+        for j in range(len(input_vec)):
+
+            #multiply each input vec value with the corresponding index of column of the matrix
+            col_sum += (input_vec[j] * weights_matrix[j][i])
+
+        #add the column total to the output vector
+        output.append(col_sum)
+
+    return output
+
+#multiple input and weights, then add bias
+def fc_linear_layer(x, w, b):
+    
+    output = []
+
+    #multiply input with weights
+    dot = matmul(x, w)
+
+    #add bias to each value in the resulting vector
+    for i in range(len(dot)):
+        output.append(dot[i] + b[i])
+
+    return output
